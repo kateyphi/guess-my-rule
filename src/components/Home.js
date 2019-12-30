4bfda7d65f5cf2d023d6e3e96320bc2924309a29
kateyphi <kateyphi@gmail.com>
2019-12-30 17:46:35 -0500

    Start with create-react-app

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ class Home extends Component {
   render(){
     return (
       <div className="component">
-        <p>This is a game where you input numbers, I do something to them according to a rule, and I ouput numbers, and you have to gues what my rule was. Get it?</p>
+        <p>To play this game, you give the function machine a number. It will <b>do something</b> to that number, and then tell you the result of what it did. You keep feeding the machine numbers, and it will keep applying the same <b>rule</b> to them and giving you the ouputs. Once you think you know what the machine is doing to your inputs each time, go ahead and <b>guess my rule!</b></p>
+        <p>To begin, click on the <b>Level</b> you'd like to play above. </p>
       </div>
     )
   }
